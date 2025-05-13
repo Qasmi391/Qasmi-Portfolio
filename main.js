ac8ff8627de5d620414cc7a5c9f44f19eb095b4a
Qasmi391 <abodyaalk391@gmail.com>
2025-05-13 08:28:55 +0300

    new

--- main.js
+++ main.js
@@ -9,3 +9,9 @@ window.onscroll = function(){
         header.classList.remove("active")
     }
 }
+
+let nav_links = document.getElementById("links");
+
+function Open_close_menu() {
+    nav_links.classList.toggle("active")
+}
